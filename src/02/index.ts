53a7048d300e0bd9946a6114d4006f8fb67e2737
lflower1206 <lflower1206@gmail.com>
2023-12-17 23:07:22 +0800

    feat: complete 2. Add Two Numbers

--- src/02/index.ts
+++ src/02/index.ts
@@ -1,5 +1,40 @@
-const addTwoNumber = (): number => {
-  return 0
+export class ListNode {
+  val: number;
+  next: ListNode | null;
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = val ?? 0;
+    this.next = next ?? null;
+  }
 }
+const addTwoNumbers = (
+  l1: ListNode | null,
+  l2: ListNode | null
+): ListNode | null => {
+  if (l1 === null && l2 === null) {
+    return null;
+  }
 
-export default addTwoNumber
+  const root = new ListNode();
+  const defaultListNode = new ListNode();
+  let tail = root;
+  let carry = 0;
+  let currentL1 = l1;
+  let currentL2 = l2;
+
+  while (carry > 0 || currentL1 !== null || currentL2 !== null) {
+    currentL1 = currentL1 ?? defaultListNode;
+    currentL2 = currentL2 ?? defaultListNode;
+
+    const value = currentL1.val + currentL2.val + carry;
+    const hasCarry = value > 9;
+
+    carry = hasCarry ? 1 : 0;
+    tail = tail.next = new ListNode(hasCarry ? value % 10 : value);
+    currentL1 = currentL1.next;
+    currentL2 = currentL2.next;
+  }
+
+  return root.next;
+};
+
+export default addTwoNumbers;
