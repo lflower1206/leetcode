f0e49f0804ba4528aa8682b755e4c3ec18d2bf9b
william <lflower1206@gmail.com>
2024-04-07 18:12:16 +0800

    feat: complete 695. Max Area of Island (#122)

---
+++ src/medium/695_Max_Area_of_Island/index.ts
@@ -0,0 +1,83 @@
+interface BfsParams {
+  grid: number[][];
+  rowIndex: number;
+  columnIndex: number;
+  counter: [number];
+  result: [number];
+}
+
+const bfs = (params: BfsParams): void => {
+  const { grid, rowIndex, columnIndex, counter, result } = params;
+  const rowLength = grid.length;
+  const columnLength = grid[0].length;
+
+  if (
+    rowIndex < 0 ||
+    columnIndex < 0 ||
+    rowIndex >= rowLength ||
+    columnIndex >= columnLength ||
+    grid[rowIndex][columnIndex] === 0
+  ) {
+    return;
+  }
+
+  grid[rowIndex][columnIndex] = 0;
+  counter[0] = counter[0] + 1;
+  result[0] = Math.max(result[0], counter[0]);
+
+  bfs({
+    grid,
+    rowIndex: rowIndex + 1,
+    columnIndex,
+    counter,
+    result,
+  });
+  bfs({
+    grid,
+    rowIndex: rowIndex - 1,
+    columnIndex,
+    counter,
+    result,
+  });
+  bfs({
+    grid,
+    rowIndex,
+    columnIndex: columnIndex + 1,
+    counter,
+    result,
+  });
+  bfs({
+    grid,
+    rowIndex,
+    columnIndex: columnIndex - 1,
+    counter,
+    result,
+  });
+};
+
+const maxAreaOfIsland = (grid: number[][]): number => {
+  const rowLength = grid.length;
+  const columnLength = grid[0].length;
+
+  const result: [number] = [0];
+
+  for (let rowIndex = 0; rowIndex < rowLength; rowIndex++) {
+    for (let columnIndex = 0; columnIndex < columnLength; columnIndex++) {
+      if (grid[rowIndex][columnIndex] === 1) {
+        const counter: [number] = [0];
+
+        bfs({
+          grid,
+          rowIndex,
+          columnIndex,
+          counter,
+          result,
+        });
+      }
+    }
+  }
+
+  return result[0];
+};
+
+export default maxAreaOfIsland;
